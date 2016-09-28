1f756de56dd33fccf4100c41d6f7174e7fffdea7
HeardWang <1834431691@qq.com>
2016-09-28 18:28:57 +0800

    Add files via upload

---
+++ js/SyntaxSetting.js
@@ -0,0 +1,7 @@
+/**
+ * Created by xz on 2016/9/27.
+ */
+SyntaxHighlighter.defaults['toolbar'] = false;
+/*SyntaxHighlighter.defaults['auto-links'] = false;*/
+SyntaxHighlighter.defaults['class-name'] = 'abc';
+SyntaxHighlighter.all();
